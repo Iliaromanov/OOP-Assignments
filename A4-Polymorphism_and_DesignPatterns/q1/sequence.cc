022f9600c51f210437a78169dafea119b7c5f222
Iliaromanov <iliaromanov033@gmail.com>
2021-11-03 21:42:46 -0400

    Func constructor

--- A4-Polymorphism_and_DesignPatterns/q1/sequence.cc
+++ A4-Polymorphism_and_DesignPatterns/q1/sequence.cc
@@ -2,6 +2,8 @@
 #include "sequence.h"
 using namespace std;
 
+Func::Func(char operation, int readIn) : operation{operation}, readIn{readIn} {}
+
 void Sequence::updateAndPrintX() {
     int result = x;
     for (auto func : funcSequence) {
