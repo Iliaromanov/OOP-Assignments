5460fc0e97bc3d4591a8aecf216f6582c9e07d22
Iliaromanov <iliaromanov033@gmail.com>
2021-11-09 13:28:51 -0500

    fix; properly attaching decorators

--- A4-Polymorphism_and_DesignPatterns/q2-ASCII_art_animation/main.cc
+++ A4-Polymorphism_and_DesignPatterns/q2-ASCII_art_animation/main.cc
@@ -30,25 +30,25 @@ int main () {
       int top, bottom, left, right;
       char what;
       std::cin >> top >> bottom >> left >> right >> what;
-      canvas = new Filledbox{top, bottom, left, right, what, canvas};
+      s.picture() = new Filledbox{top, bottom, left, right, what, s.picture()};
     }
     else if (command == "blinkingbox") {
       int top, bottom, left, right;
       char what;
       std::cin >> top >> bottom >> left >> right >> what;
-      canvas = new Blinkingbox{top, bottom, left, right, what, canvas};
+      s.picture() = new Blinkingbox{top, bottom, left, right, what, s.picture()};
     }
     else if (command == "movingbox") {
       int top, bottom, left, right;
       char what, dir;
       std::cin >> top >> bottom >> left >> right >> what >> dir;
-      canvas = new Movingbox{top, bottom, left, right, what, dir, canvas};
+      s.picture() = new Movingbox{top, bottom, left, right, what, dir, s.picture()};
     }
     else if (command == "maskbox") {
       int top, bottom, left, right;
       char what;
       std::cin >> top >> bottom >> left >> right >> what;
-      canvas = new Maskbox{top, bottom, left, right, what, canvas};
+      s.picture() = new Maskbox{top, bottom, left, right, what, s.picture()};
     }
   }
 }
