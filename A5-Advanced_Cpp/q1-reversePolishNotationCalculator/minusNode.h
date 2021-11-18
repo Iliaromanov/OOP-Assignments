40a8313089fd76a2e7a922e5f8931b1c446437c6
Iliaromanov <iliaromanov033@gmail.com>
2021-11-17 20:43:53 -0500

    concrete element header

---
+++ A5-Advanced_Cpp/q1-reversePolishNotationCalculator/minusNode.h
@@ -0,0 +1,11 @@
+#ifndef MINUSNODE_H
+#define MINUSNODE_H
+#include <memory>
+#include "expressionNode.h"
+
+class MinusNode : public ExpressionNode { // Concrete element
+    public:
+        virtual std::string accept(ExpressionVisitor v);
+};
+
+#endif
