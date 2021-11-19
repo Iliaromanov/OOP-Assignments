11c31c6709dd6e8ca008645bebe229943d4dadb2
Iliaromanov <iliaromanov033@gmail.com>
2021-11-18 19:59:11 -0500

    added ctor to NumNode

--- A5-Advanced_Cpp/q1-reversePolishNotationCalculator/numNode.cc
+++ A5-Advanced_Cpp/q1-reversePolishNotationCalculator/numNode.cc
@@ -1,7 +1,9 @@
 #include "numNode.h"
+#include "expressionVisitor.h"
 using namespace std;
 
-int NumNode::getNum() { return num; }
+NumNode::NumNode(std::string num) : num{num} {}
+string NumNode::getNum() { return num; }
 string NumNode::accept(ExpressionVisitor &v) {
-
+    return v.visit(this);
 }
