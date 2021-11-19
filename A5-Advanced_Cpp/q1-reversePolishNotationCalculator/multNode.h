f0d318a545f1e371acb0c29a5565e3105acadae1
Iliaromanov <iliaromanov033@gmail.com>
2021-11-18 19:59:43 -0500

    added ctor to header

--- A5-Advanced_Cpp/q1-reversePolishNotationCalculator/multNode.h
+++ A5-Advanced_Cpp/q1-reversePolishNotationCalculator/multNode.h
@@ -5,7 +5,8 @@
 
 class MultNode : public ExpressionNode { // Concrete element
     public:
-        virtual std::string accept(ExpressionVisitor v);
+        virtual std::string accept(ExpressionVisitor &v) override;
+        MultNode(std::istringstream &expression_ss);
 };
 
 #endif
