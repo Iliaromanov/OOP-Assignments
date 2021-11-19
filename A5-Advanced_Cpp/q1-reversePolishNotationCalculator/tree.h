8f6c56fb412bf653383ccda17f823339c240c526
Iliaromanov <iliaromanov033@gmail.com>
2021-11-19 13:34:12 -0500

    update tree header

--- A5-Advanced_Cpp/q1-reversePolishNotationCalculator/tree.h
+++ A5-Advanced_Cpp/q1-reversePolishNotationCalculator/tree.h
@@ -4,13 +4,18 @@
 #include <sstream>
 #include "expressionNode.h"
 #include <string>
+#include <vector>
 
 class Tree {
     std::unique_ptr<ExpressionNode> root;
+    std::vector<std::unique_ptr<ExpressionNode>> stack;
     public:
-        ExpressionNode *getRoot();
-        Tree(std::istringstream &expression_ss, std::string root_val); // ctor that builds tree from ss
+        Tree();
         ~Tree();
+        // method that builds tree from ss
+        void BuildTree(std::istringstream &expression_ss, std::string root_val);
+        // getter for root node
+        ExpressionNode *getRoot();
 };
 
 #endif
