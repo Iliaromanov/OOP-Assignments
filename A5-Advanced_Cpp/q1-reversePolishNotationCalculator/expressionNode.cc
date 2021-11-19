e8244ce89dcf0e31c24d07c252844de556717cee
Iliaromanov <iliaromanov033@gmail.com>
2021-11-19 13:35:19 -0500

    moved tree building algorithm to tree.cc

--- A5-Advanced_Cpp/q1-reversePolishNotationCalculator/expressionNode.cc
+++ A5-Advanced_Cpp/q1-reversePolishNotationCalculator/expressionNode.cc
@@ -2,40 +2,12 @@
 #include "expressionVisitor.h"
 using namespace std;
 
+
 ExpressionNode::ExpressionNode() : left{nullptr}, right{nullptr} {} // default ctor
 
-ExpressionNode::ExpressionNode(istringstream &expression_ss) { // ctor for building tree from ss
-    string right_val;
-    string left_val;
-    expression_ss >> right_val;
-    expression_ss >> left_val;
-
-    // build right subtree
-    if (right_val == "+") {
-        right = make_unique<PlusNode>(expression_ss);
-    } else if (right_val == "-") {
-        right = make_unique<MinusNode>(expression_ss);
-    } else if (right_val == "*") {
-        right = make_unique<MultNode>(expression_ss);
-    } else if (right_val == "/") {
-        right = make_unique<DivNode>(expression_ss);
-    } else { // NumNode otherwise
-        right = make_unique<NumNode>(right_val);
-    }
-
-    // build left subtree
-    if (left_val == "+") {
-        left = make_unique<PlusNode>(expression_ss);
-    } else if (left_val == "-") {
-        left = make_unique<MinusNode>(expression_ss);
-    } else if (left_val == "*") {
-        left = make_unique<MultNode>(expression_ss);
-    } else if (left_val == "/") {
-        left = make_unique<DivNode>(expression_ss);
-    } else { // NumNode otherwise
-        left = make_unique<NumNode>(right_val);
-    }
-}
+// ctor that takes left and right child nodes
+ExpressionNode::ExpressionNode(unique_ptr<ExpressionNode> left, unique_ptr<ExpressionNode> right) 
+    : left{move(left)}, right{move(right)} {}
 
 // getters
 ExpressionNode *ExpressionNode::getLeft() { return left.get(); }
