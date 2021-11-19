c342ceae16647324cceb3ac3c7c7ad8f2e901031
Iliaromanov <iliaromanov033@gmail.com>
2021-11-18 20:02:03 -0500

    updated header

--- A5-Advanced_Cpp/q1-reversePolishNotationCalculator/expressionNode.h
+++ A5-Advanced_Cpp/q1-reversePolishNotationCalculator/expressionNode.h
@@ -1,7 +1,10 @@
 #ifndef EXPRNODE_H
 #define EXPRNODE_H
+#include <sstream>
 #include <memory>
-#include "expressionVisitor.h"
+#include <string>
+
+class ExpressionVisitor;
 
 class ExpressionNode { // Element abstract base class
     std::unique_ptr<ExpressionNode> left;
@@ -9,7 +12,9 @@ class ExpressionNode { // Element abstract base class
     public:
         ExpressionNode *getLeft();
         ExpressionNode *getRight();
-        virtual std::string accept(ExpressionVisitor v) = 0;
+        virtual std::string accept(ExpressionVisitor &v) = 0;
+        ExpressionNode();
+        ExpressionNode(std::istringstream &expression_ss);
         ~ExpressionNode();
 };
 
