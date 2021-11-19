22fe30cd1bcfed2f740e284a60e0e3ad6729756f
Iliaromanov <iliaromanov033@gmail.com>
2021-11-18 19:58:18 -0500

    add destructor

--- A5-Advanced_Cpp/q1-reversePolishNotationCalculator/parse.h
+++ A5-Advanced_Cpp/q1-reversePolishNotationCalculator/parse.h
@@ -1,5 +1,6 @@
 #ifndef PARSE_H
 #define PARSE_H
+#include "expressionVisitor.h"
 #include "plusNode.h"
 #include "minusNode.h"
 #include "multNode.h"
@@ -8,11 +9,12 @@
 
 class Parse : public ExpressionVisitor { // Concrete visitor
     public:
-        std::string visit(PlusNode *node);
-        std::string visit(MinusNode *node);
-        std::string visit(MultNode *node);
-        std::string visit(DivNode *node);
-        std::string visit(NumNode *node);
+        std::string visit(PlusNode *node) override;
+        std::string visit(MinusNode *node) override;
+        std::string visit(MultNode *node) override;
+        std::string visit(DivNode *node) override;
+        std::string visit(NumNode *node) override;
+        ~Parse();
 };
 
 #endif
